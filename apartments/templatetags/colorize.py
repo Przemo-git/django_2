c1590df83087746e3e0064f5abe505419c792cd4
Przemo-git <59745225+Przemo-git@users.noreply.github.com>
2022-02-07 06:40:03 +0100

    Update colorize.py

--- apartments/templatetags/colorize.py
+++ apartments/templatetags/colorize.py
@@ -6,9 +6,19 @@ from django.utils.safestring import mark_safe
 
 
 
+
+# @register.filter
+# def colorize(val):
+#     mark = str(val)[2:]
+#     if mark < str(val)[2:]:
+#         html_string = f"<span style='color:black'>{val} zł</span"
+#     else:
+#         html_string = f"<span style='color:green'>{val} zł</span"
+#     return mark_safe(html_string)
+
 @register.filter
 def colorize(val):
-    mark = str(val)[2:]
+    mark = str(val)[2]
     if mark < str(val)[2:]:
         html_string = f"<span style='color:black'>{val} zł</span"
     else:
