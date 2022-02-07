05e83c98e50fe9e5bb19b6885be62e1f4459028a
Przemo-git <59745225+Przemo-git@users.noreply.github.com>
2022-02-07 06:59:37 +0100

    Update colorize.py

--- apartments/templatetags/colorize.py
+++ apartments/templatetags/colorize.py
@@ -1,5 +1,7 @@
 from django import template
 
+from apartments.models import Apartment
+
 register = template.Library()
 
 from django.utils.safestring import mark_safe
@@ -7,20 +9,12 @@ from django.utils.safestring import mark_safe
 
 
 
-# @register.filter
-# def colorize(val):
-#     mark = str(val)[2:]
-#     if mark < str(val)[2:]:
-#         html_string = f"<span style='color:black'>{val} zł</span"
-#     else:
-#         html_string = f"<span style='color:green'>{val} zł</span"
-#     return mark_safe(html_string)
-
 @register.filter
 def colorize(val):
-    mark = str(val)[2]
-    if mark < str(val)[2:]:
-        html_string = f"<span style='color:black'>{val} zł</span"
-    else:
-        html_string = f"<span style='color:green'>{val} zł</span"
-    return mark_safe(html_string)
+    if Apartment.objects.filter(activated=True):
+        mark = str(val)[2]
+        if mark < str(val)[2:]:
+            html_string = f"<span style='color:black'>{val} zł</span"
+        else:
+            html_string = f"<span style='color:green'>{val} zł</span"
+        return mark_safe(html_string)
